e9545c393dff25e3df88ed4defee813f83448f6c
DESKTOP-GLKRT82\thoma <thomas.mathis.mt@gmail.com>
2025-12-06 22:30:17 +0100

    Initial commit

---
+++ HiddenStash/Main.cs
@@ -0,0 +1,26 @@
+﻿using TaleWorlds.MountAndBlade;
+
+
+namespace HiddenStash
+{
+    public class Main : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
